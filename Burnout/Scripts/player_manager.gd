89d3b5a99beddb15abc9f9bcd67fa3c6eacbb164
lvn-p <5nd2phwhhz@privaterelay.appleid.com>
2026-09-19 21:39:27 +0200

    init project

---
+++ Burnout/Scripts/player_manager.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+# Player Stats
+
+@export var starting_energy = 10.0
+@export var starting_sanity = 10.0
+@export var starting_money = 10.0
+
+var current_energy = 10.0
+var current_sanity = 10.0
+var current_money = 0.0
+ 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	current_energy = starting_energy
+	current_sanity = starting_sanity
+	current_money = starting_money
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func affect_stats(sanity: float, energy: float, money: float) -> void:
+	current_energy += sanity
+	current_sanity += energy
+	current_money += money
